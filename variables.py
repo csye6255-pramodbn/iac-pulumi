2b22d60ffe9f468fc7aaf88f5d0c25d43671bbed
pramodbn <begurnagaraj.p@northeastern.edu>
2023-10-09 17:50:18 -0400

    variables

---
+++ variables.py
@@ -0,0 +1,8 @@
+# Variables
+profile = "dev"
+region = "us-west-2"
+
+vpc_name = "My-VPC"
+vpc_cidr = "10.0.0.0/16"
+public_subnet_cidr = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+private_subnet_cidr = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
