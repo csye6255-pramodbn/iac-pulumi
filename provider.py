5fe8ce7d6cf3da409486a13e21aac8da6c9f5464
pramodbn <begurnagaraj.p@northeastern.edu>
2023-10-12 15:53:50 -0400

    provider-resolve

--- provider.py
+++ provider.py
@@ -1,7 +1,6 @@
 from pulumi import Config
 import pulumi_aws as aws
-config1 = Config("aws")
 
-aws_profile = config1.get("profile")
-
+config = Config("aws")
+aws_profile = config.get("profile")
 provider = aws.Provider("provider", profile=aws_profile)
