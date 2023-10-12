f71eac933e7dc1450980eea8336f6ca0f7ef3a90
PRAMOD BEGUR NAGARAJ <114458145+pramodneu@users.noreply.github.com>
2023-10-12 08:48:30 -0400

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,8 +1,2 @@
 import myVPC
 import outputs
-
-# from variables import cli_private_subnets_cidr, cli_public_subnets_cidr
-
-
-# print("abcd", cli_public_subnets_cidr[0])
-# print(cli_private_subnets_cidr[1])
