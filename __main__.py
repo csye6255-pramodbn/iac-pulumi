29e45955be905c9b169e286de8ab205573e9a5a6
pramodbn <begurnagaraj.p@northeastern.edu>
2023-11-26 14:18:00 -0500

    email-fix

--- __main__.py
+++ __main__.py
@@ -1,18 +1,18 @@
-import pulumi
-# import provider  # Import provider module
-# opts = pulumi.ResourceOptions(provider=provider.provider) # Pass provider to resource options
-import myVPC
-import fetch_myip
-import ports
-import application_security_group
-import load_balancer
-import iam_policy
-import cloudwatch_group_stream
+# import pulumi
+# # import provider  # Import provider module
+# # opts = pulumi.ResourceOptions(provider=provider.provider) # Pass provider to resource options
+# import myVPC
+# import fetch_myip
+# import ports
+# import application_security_group
+# import load_balancer
+# import iam_policy
+# import cloudwatch_group_stream
 import gcloud
 import dynamodb
 import aws_lambda
 import sns
-import rds_postgress
-import auto_scaling_group
-import hosted_zone
-import outputs
+# import rds_postgress
+# import auto_scaling_group
+# import hosted_zone
+# import outputs
